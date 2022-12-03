53443b5ba3a996d2db723c15cf073e7733dc4b37
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-12-03 17:17:30 +0100

    File theme.ts has been created.

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,22 @@ const Container = styled.div`
   display: flex;
 `;
 const Main = styled.div`
-    flex: 7;
+  flex: 7;
+  background-color: #181818;
 `;
 const Wrapper = styled.div`
 `;
 
 export const App = () => {
     return (
-        <Container className="app">
-            <Menu/>
-            <Main>
-                <Navbar/>
-                <Wrapper>
-                    Video Cards
-                </Wrapper>
-            </Main>
-        </Container>
+            <Container className="app">
+                <Menu/>
+                <Main>
+                    <Navbar/>
+                    <Wrapper>
+                        Video Cards
+                    </Wrapper>
+                </Main>
+            </Container>
     );
 }
