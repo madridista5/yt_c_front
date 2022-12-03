4d7fb7162f943fc408af7ff96f1eaf10badc07fe
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-12-03 16:43:47 +0100

    The icons have been downloaded.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import logo from '../../assets/logo.png';
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SettingsIcon from '@mui/icons-material/Settings';
+import FlagIcon from '@mui/icons-material/Flag';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import MovieCreationIcon from '@mui/icons-material/MovieCreation';
+
 
 const Container = styled.div`
   flex: 1;
@@ -23,7 +39,20 @@ const Logo = styled.div`
 `;
 
 const Img = styled.img`
-    height: 25px;
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0;
+`;
+
+const Hr = styled.hr`
+    margin: 15px 0;
+  border: 0.5px solid #373737;
 `;
 
 export const Menu = () => (
@@ -33,6 +62,69 @@ export const Menu = () => (
                 <Img src={logo}/>
                 CloneTube
             </Logo>
+            <Item>
+                <HomeIcon/>
+                Home
+            </Item>
+            <Item>
+                <ExploreIcon/>
+                Explore
+            </Item>
+            <Item>
+                <SubscriptionsIcon/>
+                Subscriptions
+            </Item>
+            <Hr/>
+            <Item>
+                <VideoLibraryIcon/>
+                Library
+            </Item>
+            <Item>
+                <HistoryIcon/>
+                History
+            </Item>
+            <Hr/>
+            <Item>
+                <LibraryMusicIcon/>
+                Music
+            </Item>
+            <Item>
+                <SportsSoccerIcon/>
+                Sports
+            </Item>
+            <Item>
+                <SportsEsportsIcon/>
+                Gaming
+            </Item>
+            <Item>
+                <MovieCreationIcon/>
+                Movies
+            </Item>
+            <Item>
+                <NewspaperIcon/>
+                News
+            </Item>
+            <Item>
+                <LiveTvIcon/>
+                Live
+            </Item>
+            <Hr/>
+            <Item>
+                <SettingsIcon/>
+                Settings
+            </Item>
+            <Item>
+                <FlagIcon/>
+                Report
+            </Item>
+            <Item>
+                <HelpOutlineIcon/>
+                Help
+            </Item>
+            <Item>
+                <LightModeIcon/>
+                Light Mode
+            </Item>
         </Wrapper>
     </Container>
 )
