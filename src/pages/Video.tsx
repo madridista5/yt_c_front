df27c78227144ed526c6624e245c7b97c9c5c9c6
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-12-05 15:09:31 +0100

    The structure of Video Component has been prepared.

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,8 +1,65 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const VideoWrapper = styled.div`
+
+`;
+
+const Title = styled.h1`
+
+`;
+
+const Details = styled.div`
+
+`;
+
+const Info = styled.span`
+
+`;
+
+const Buttons = styled.div`
+
+`;
+
+const Button = styled.div`
+
+`;
+
+const Recommendation = styled.div`
+  flex: 2;
+`;
 
 export const Video = () => {
-    return <Container>Video</Container>
+    return <Container>
+        <Content>
+            <VideoWrapper>
+                <iframe
+                    width="100%"
+                    height="720"
+                    src="https://youtube.com/embed/k3Vfj-e1Ma4"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                />
+            </VideoWrapper>
+            <Title>Test video</Title>
+            <Details>
+                <Info>7.948.244 views * Jun 22, 2022</Info>
+                <Buttons>
+                    <Button>button</Button>
+                </Buttons>
+            </Details>
+        </Content>
+        <Recommendation>Recommendation</Recommendation>
+    </Container>
 }
