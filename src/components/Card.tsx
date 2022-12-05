4968da038507a461df40e53ca7cc308d4ab7efec
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-12-05 13:13:31 +0100

    Image has been created.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-    width: 360px;
+  width: 360px;
+  margin-bottom: 45px;
+  cursor: pointer;
+`;
+
+const Image  = styled.img`
+  width: 100%;
+  height: 202px; // from YT
+  background-color: #999;
 `;
 
 export const Card = () => {
-    return <Container>Card</Container>
+    return <Container><Image/></Container>
 }
