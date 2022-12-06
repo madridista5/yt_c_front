3feb6d97be587ceddc046fe41615fe9276eadc32
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-12-06 11:15:30 +0100

    Component Recommendation has been finished.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,28 +2,33 @@ import React from "react";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
-const Container = styled.div`
-  width: 360px;
-  margin-bottom: 45px;
+const Container = styled.div<{ type: string | undefined }>`
+  width: ${(props => props.type !== 'sm' && '360px')};
+  margin-bottom: ${(props => props.type === 'sm' ? '10px' : '45px')};
   cursor: pointer;
+  display: ${(props => props.type === 'sm' && 'flex')};
+  gap: 10px;
 `;
 
-const Image = styled.img`
+const Image = styled.img<{ type: string | undefined }>`
   width: 100%;
-  height: 202px; // from YT
+  height: ${(props => props.type === 'sm' ? '120px' : '204px')}; // from YT
   background-color: #999;
+  flex: 1;
 `;
 
-const Details = styled.div`
+const Details = styled.div<{ type: string | undefined }>`
   display: flex;
-  margin-top: 16px;
+  margin-top: ${(props => props.type !== 'sm' && '16px')};
   gap: 12px;
+  flex: 1;
 `;
 
-const ChannelImage = styled.img`
+const ChannelImage = styled.img<{ type: string | undefined }>`
   width: 36px;
   height: 36px;
   border-radius: 50%;
+  display: ${(props => props.type === 'sm' && 'none')};
 `;
 
 const Texts = styled.div``;
@@ -45,12 +50,16 @@ const Info = styled.div`
   color: ${({theme}) => theme.textSoft};
 `;
 
-export const Card = () => {
+interface Props {
+    type?: string,
+}
+
+export const Card = ({type}: Props) => {
     return <Link to="/video/test" style={{textDecoration: 'none'}}>
-        <Container>
-            <Image src="https://cdn.pixabay.com/photo/2016/08/27/12/06/website-1624028_960_720.png"/>
-            <Details>
-                <ChannelImage src="https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_960_720.png"/>
+        <Container type={type}>
+            <Image type={type} src="https://cdn.pixabay.com/photo/2016/08/27/12/06/website-1624028_960_720.png"/>
+            <Details type={type}>
+                <ChannelImage type={type} src="https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_960_720.png"/>
                 <Texts>
                     <Title>Title video</Title>
                     <ChannelName>Channel name</ChannelName>
